52078a0aeee2fc89fc0f213acca720b6beb6fad5
ElyBry <150510586+ElyBry@users.noreply.github.com>
2024-10-25 00:13:55 +0500

    Start to add media for mobile gadgets

--- resources/js/components/_header.tsx
+++ resources/js/components/_header.tsx
@@ -59,79 +59,104 @@ export default ({className}) => {
         setIsOpen(!isOpen);
     };
 
+    const [openBurgerMenu, setOpenBurgerMenu] = useState(false);
+
     return  (
-        <header className={`${styles.header} ${className}`}>
-            <Link to={"/"}>
-                <div id={"logo"} className={styles.logo}>
-                    <img src={"../../image/logos/logo.png"} alt={"Доставка фруктов"}/>
-                    Фруктовый мир
-                </div>
-            </Link>
-            {isAuth ?
-                <>
-                    <div id={"hHead"} className={styles.hHead}>
-                        {checkRole(['Super Admin', 'Admin', 'Manager']) && (
-                            <div className={styles.dropdown}>
-                                <div className={styles.hSection} onClick={toggleMenu}>
-                                    Админ Панель
-                                </div>
-                                {isOpen && (
-                                    <div className={styles.dropdownContent}>
-                                        <Link to={"/admin/orders"}>
-                                            <div className={styles.hSection}>Заказы</div>
-                                        </Link>
-                                        <Link to={"/admin/users"}>
-                                            <div className={styles.hSection}>Пользователи</div>
-                                        </Link>
-                                        <Link to={"/admin/products"}>
-                                            <div className={styles.hSection}>Продукты</div>
-                                        </Link>
-                                        <Link to={"/admin/feedbacksProducts"}>
-                                            <div className={styles.hSection}>Отзывы о продуктах</div>
-                                        </Link>
-                                        <Link to={"/admin/feedbacks"}>
-                                            <div className={styles.hSection}>Отзывы о приложении</div>
-                                        </Link>
+        <>
+            <div className={`${styles.burgerMenu} ${openBurgerMenu ? styles.visible : ""}`}>
+                <nav>
+                    <ul>
+                        <li>
+                            Главная
+                        </li>
+                        <li>
+                            Продукты
+                        </li>
+                        <li>
+                            Войти
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <header className={`${styles.header} ${className}`}>
+                <Link to={"/"}>
+                    <div id={"logo"} className={styles.logo}>
+                        <img src={"../../image/logos/logo.png"} alt={"Доставка фруктов"}/>
+                        Фруктовый мир
+                    </div>
+                </Link>
+                {isAuth ?
+                    <>
+                        <div id={"hHead"} className={styles.hHead}>
+                            {checkRole(['Super Admin', 'Admin', 'Manager']) && (
+                                <div className={styles.dropdown}>
+                                    <div className={styles.hSection} onClick={toggleMenu}>
+                                        Админ Панель
                                     </div>
-                                )}
+                                    {isOpen && (
+                                        <div className={styles.dropdownContent}>
+                                            <Link to={"/admin/orders"}>
+                                                <div className={styles.hSection}>Заказы</div>
+                                            </Link>
+                                            <Link to={"/admin/users"}>
+                                                <div className={styles.hSection}>Пользователи</div>
+                                            </Link>
+                                            <Link to={"/admin/products"}>
+                                                <div className={styles.hSection}>Продукты</div>
+                                            </Link>
+                                            <Link to={"/admin/feedbacksProducts"}>
+                                                <div className={styles.hSection}>Отзывы о продуктах</div>
+                                            </Link>
+                                            <Link to={"/admin/feedbacks"}>
+                                                <div className={styles.hSection}>Отзывы о приложении</div>
+                                            </Link>
+                                        </div>
+                                    )}
+                                </div>
+                            )}
+                            <Link to={"/products"}>
+                                <div className={styles.hSection}>Заказать продукты</div>
+                            </Link>
+                            <Link to={"/orders"}>
+                                <div className={styles.hSection}>Заказы</div>
+                            </Link>
+                            <div onClick={() => logout()} className={styles.hSection}>Выйти</div>
+                        </div>
+                        <div id={"burger"} className={styles.burger}>
+                            <div className={`${styles.burgerImg} ${openBurgerMenu ? styles.visible : ""}`}
+                                 onClick={() => setOpenBurgerMenu(!openBurgerMenu)}
+                            >
+                                <img src={"../../image/icons/elements/burger-menu-right.svg"}/>
                             </div>
-                        )}
-                        <Link to={"/products"}>
-                            <div className={styles.hSection}>Заказать продукты</div>
-                        </Link>
-                        <Link to={"/orders"}>
-                            <div className={styles.hSection}>Заказы</div>
-                        </Link>
-                        <div onClick={() => logout()} className={styles.hSection}>Выйти</div>
-                    </div>
-                    <div id={"burger"} className={styles.burger}>
-                        <img src={"../../image/icons/elements/burger-menu-right.svg"}/>
-                    </div>
-                </>
-                :
-                <>
-                    <div id={"hHead"} className={styles.hHead}>
-                        <Link to={"/products"}>
-                            <div className={styles.hSection}>Заказать продукты</div>
-                        </Link>
-                        <Link to={"/#guaranteeBlock"}>
-                            <div className={styles.hSection}>Гарантия</div>
-                        </Link>
-                        <Link to={"/#faqBlock"}>
-                            <div className={styles.hSection}>FAQ</div>
-                        </Link>
-                        <Link to={"/login"}>
-                            <div className={styles.hSection}>Войти</div>
-                        </Link>
-                    </div>
-                    <div id={"burger"} className={styles.burger}>
-                        <img src={"../../image/icons/elements/burger-menu-right.svg"}/>
-                    </div>
-                </>
+                        </div>
+                    </>
+                    :
+                    <>
+                        <div id={"hHead"} className={styles.hHead}>
+                            <Link to={"/products"}>
+                                <div className={styles.hSection}>Заказать продукты</div>
+                            </Link>
+                            <Link to={"/#guaranteeBlock"}>
+                                <div className={styles.hSection}>Гарантия</div>
+                            </Link>
+                            <Link to={"/#faqBlock"}>
+                                <div className={styles.hSection}>FAQ</div>
+                            </Link>
+                            <Link to={"/login"}>
+                                <div className={styles.hSection}>Войти</div>
+                            </Link>
+                        </div>
+                        <div id={"burger"} className={styles.burger}>
+                            <div className={styles.burgerImg}>
+                                <img src={"../../image/icons/elements/burger-menu-right.svg"}/>
+                            </div>
+                        </div>
+                    </>
 
-            }
+                }
+            </header>
+        </>
 
-        </header>
     );
 }
 
