fec978d4e190d4dd808c615592e3d99c9b0c32f9
ElyBry <150510586+ElyBry@users.noreply.github.com>
2024-11-11 02:21:44 +0500

    Add Tg API

--- resources/js/components/TelegramConnect/TelegramConnect.tsx
+++ resources/js/components/TelegramConnect/TelegramConnect.tsx
@@ -9,7 +9,7 @@ const Telegram = () => {
     useEffect(() => {
         const script = document.createElement('script');
         script.src = "https://telegram.org/js/telegram-widget.js?22";
-        script.setAttribute('data-telegram-login', '@Fructs_EKB_bot');
+        script.setAttribute('data-telegram-login', 'Fructs_EKB_bot');
         script.setAttribute('data-size', 'medium');
         script.setAttribute('data-onauth', 'onTelegramAuth(user)');
         script.setAttribute('data-request-access', 'write');
