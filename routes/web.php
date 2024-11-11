093f7b36a75fe2f3e70964c51b8c2797d148e812
ElyBry <150510586+ElyBry@users.noreply.github.com>
2024-11-11 06:13:29 +0500

    Add TG

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-Route::get('/auth/telegram/callback', [AuthController::class, 'handleTelegramCallback']);
+Route::post('/auth/telegram/callback', [AuthController::class, 'handleTelegramCallback']);
 Route::get('{reactRoutes}', function () {
     return view('main'); // your start view
 })->where('reactRoutes', '^((?!api).)*$');
