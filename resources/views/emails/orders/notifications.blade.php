7400557f8ac80caf602033326f5d8121bd959186
ElyBry <150510586+ElyBry@users.noreply.github.com>
2024-11-11 05:25:42 +0500

    Add TG

--- resources/views/emails/orders/notifications.blade.php
+++ resources/views/emails/orders/notifications.blade.php
@@ -6,7 +6,7 @@
 <body>
     <h1>Новый заказ!</h1>
     <p>У вас новый заказ с ID: {{ $orderId }}</p>
-    <p>Общая сумма заказа: {{ $orderTotal }}</p>
+    <p>Общая сумма заказа: {{ $total_price }}</p>
     <p>Адрес: {{ $address }}</p>
     <p>Номер телефона: {{ $number }}</p>
     <h2>Подробнее на <a href="https://fructs.ru/admin/orders">сайте</a> </h2>
