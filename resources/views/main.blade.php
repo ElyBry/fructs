c047277380510e11bcfe420279f95c2b101b4f40
ElyBry <150510586+ElyBry@users.noreply.github.com>
2025-01-10 09:26:20 +0500

    Fix long loading 2.0

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -3,7 +3,21 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Продукты</title>
+    <title>Продукты - Доставка на дом и самовывоз</title>
+    <meta name="description" content="Заказывайте свежие продукты с доставкой на дом или выберите удобный самовывоз. Широкий ассортимент, высокое качество и доступные цены." />
+    <meta name="keywords" content="доставка продуктов, самовывоз, купить продукты, свежие продукты, продукты на дом" />
+    <meta property="og:title" content="Продукты - Доставка на дом и самовывоз" />
+    <meta property="og:description" content="Удобная доставка свежих продуктов на дом или самовывоз из наших точек. Ваши любимые товары всегда под рукой!" />
+    <meta property="og:image" content="image/logos/logo.webp" />
+    <meta property="og:url" content="fructs.ru" />
+    <meta property="og:type" content="website" />
+    <meta name="robots" content="index, follow" />
+    <meta name="language" content="ru" />
+    <meta name="theme-color" content="#ffffff" />
+    <link rel="preload" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" as="style" onload="this.onload=null;this.rel='stylesheet'"/>
+    <link rel="preload" href="https://fonts.googleapis.com/css2?family=Pacifico&family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap" as="style" onload="this.onload=null;this.rel='stylesheet'"/>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0"/>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Pacifico&family=Roboto+Mono:ital,wght@0,100..700;1,100..700&display=swap"/>
     @viteReactRefresh
     @vite(['resources/js/App.tsx'])
 </head>
